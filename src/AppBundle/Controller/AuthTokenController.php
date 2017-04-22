a20cfbc72300d6795f0ee125e6f183a3e274d662
rhidja <rhidja@gmail.com>
2017-04-22 23:34:58 +0200

    Sécurisation de l'API

---
+++ src/AppBundle/Controller/AuthTokenController.php
@@ -0,0 +1,62 @@
+<?php
+# src/AppBundle/Controller/AuthTokenController.php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use FOS\RestBundle\Controller\Annotations as Rest; // alias pour toutes les annotations
+use AppBundle\Form\Type\CredentialsType;
+use AppBundle\Entity\AuthToken;
+use AppBundle\Entity\Credentials;
+
+class AuthTokenController extends Controller
+{
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post("/auth-tokens")
+     */
+    public function postAuthTokensAction(Request $request)
+    {
+        $credentials = new Credentials();
+        $form = $this->createForm(CredentialsType::class, $credentials);
+
+        $form->submit($request->request->all());
+
+        if (!$form->isValid()) {
+            return $form;
+        }
+
+        $em = $this->get('doctrine.orm.entity_manager');
+
+        $user = $em->getRepository('AppBundle:User')
+            ->findOneByEmail($credentials->getLogin());
+
+        if (!$user) { // L'utilisateur n'existe pas
+            return $this->invalidCredentials();
+        }
+
+        $encoder = $this->get('security.password_encoder');
+        $isPasswordValid = $encoder->isPasswordValid($user, $credentials->getPassword());
+
+        /*if (!$isPasswordValid) { // Le mot de passe n'est pas correct
+            return $this->invalidCredentials();
+        }*/
+
+        $authToken = new AuthToken();
+        $authToken->setValue(base64_encode(random_bytes(50)));
+        $authToken->setCreatedAt(new \DateTime('now'));
+        $authToken->setUser($user);
+
+        $em->persist($authToken);
+        $em->flush();
+
+        return $authToken;
+    }
+
+    private function invalidCredentials()
+    {
+        return \FOS\RestBundle\View\View::create(['message' => 'Invalid credentials'], Response::HTTP_BAD_REQUEST);
+    }
+}
---
+++ src/AppBundle/Form/Type/CredentialsType.php
@@ -0,0 +1,24 @@
+<?php
+# src/AppBundle/Form/Type/CredentialsType.php
+namespace AppBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CredentialsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('login');
+        $builder->add('password');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => 'AppBundle\Entity\Credentials',
+            'csrf_protection' => false
+        ]);
+    }
+}
