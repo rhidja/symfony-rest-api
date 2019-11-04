c1f3f14042c0ae97369d5122938e0d25abdf95f2
Unknown <rhidja@gmail.com>
2019-11-04 22:34:26 +0100

    Remplacer le JMS Serializer par le serializer Symfony

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,11 +1,10 @@
 <?php
-// src/App/Entity/User.php
 
 namespace App\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use JMS\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity
