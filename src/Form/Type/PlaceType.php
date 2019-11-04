34c24f9b46478e04240eb233b632f2ea92d81cef
Unknown <rhidja@gmail.com>
2019-11-04 23:14:36 +0100

    Ré-implémentation du contrôleur PlaceController

--- src/Form/Type/PlaceType.php
+++ src/Form/Type/PlaceType.php
@@ -13,17 +13,12 @@ class PlaceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name', TextType::class, [
-            'description' => "Nom du lieu"
-        ]);
-        $builder->add('address', TextType::class, [
-            'description' => "Adresse complète du lieu"
-        ]);
+        $builder->add('name', TextType::class);
+        $builder->add('address', TextType::class);
         $builder->add('prices', CollectionType::class, [
             'entry_type' => PriceType::class,
             'allow_add' => true,
             'error_bubbling' => false,
-            'description' => "Liste des prix pratiqués"
         ]);
     }
 
