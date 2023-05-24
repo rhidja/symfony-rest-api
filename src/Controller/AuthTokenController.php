2cd08919571d331c7aa8d378a8bb2012abfa7ad1
Ramtane Hidja <ramtane.hidja@smile.fr>
2023-05-24 21:36:19 +0200

    Upgrade symfony from 4.4 version to 6.2 version

--- src/Controller/AuthTokenController.php
+++ src/Controller/AuthTokenController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use App\Form\Type\CredentialsType;
 use App\Entity\AuthToken;
 use App\Entity\Credentials;
